fc4cefc2f3acfbbdef3ed924b5a964dd6d8601aa
Rinat.spb34 <rinat.volg134@gmail.com>
2025-12-02 19:19:24 +0300

    RedactorUnity

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        
+        if (!Int32.TryParse(Console.ReadLine(), out var a))
+        {
+            Console.WriteLine("Not number!");
+            return;
+        }
+
+        if (!Int32.TryParse(Console.ReadLine(), out var b))
+        {
+            Console.WriteLine("Not number!");
+            return;
+        }
+        var s = Console.ReadLine();
+        var boolVar = true;
+        if (s.Length == 0 || s.Length > 1 && !boolVar) 
+        {
+            Console.WriteLine("Wrong sign");
+            return;
+        }
+        switch (s[0])
+        {
+            case '+':
+                Console.WriteLine("Result of {0} + {1} = {2} ", a, b, a + b);
+                break;
+            case '-':
+                Console.WriteLine("Result of {0} - {1} = {2} ", a, b, a - b);
+                break;
+            case '*':
+                Console.WriteLine("Result of {0} * {1} = {2} ", a, b, a * b);
+                break;
+            case '/':
+                Console.WriteLine("Result of {0} / {1} = {2} ", a, b, a / b);
+                break;
+            case '%':
+                Console.WriteLine("Result of {0} % {1} = {2} ", a, b, a % b);
+                break;
+            default:
+                Console.WriteLine("Wrong sign");
+                break;
+
+        }
+
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+    }
+}

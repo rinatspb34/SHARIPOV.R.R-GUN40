fa60fdb9c4142fb4c73cd9398b5586e9d8b29077
DESKTOP-HJU5FPI\asd <rinat.volg134@gmail.com>
2026-02-05 15:14:28 +0300

    Classes

--- Program.cs
+++ Program.cs
@@ -1,57 +1,28 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace HomeWork
+﻿namespace HomeWork
 {
+   
+   
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] fibonacci = new[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };                                                      // 1. Фибоначчи
+            Unit warrior = new Unit("Воин");
+            Console.WriteLine($"Имя: {warrior.Name}");
+            Console.WriteLine($"Здоровье: {warrior.Health}");
+            Console.WriteLine($"Урон: {warrior.Damage}");
+            Console.WriteLine($"Броня: {warrior.Armor}");
+            Console.WriteLine($"Фактическое здоровье: {warrior.GetRealHealth()}");
 
-            for (int i = 0; i < fibonacci.Length; i++) 
-            {
-                Console.WriteLine(fibonacci[i] + " ");
-            }
-            Console.WriteLine();
+            bool isDead = warrior.SetDamage(60);
+            Console.WriteLine($"Атака 60: Здоровье = {warrior.Health}, Юнит мертв: {isDead}");
+
+            isDead = warrior.SetDamage(110);
+            Console.WriteLine($"Атака 110: Здоровье = {warrior.Health}, Юнит мертв: {isDead}");
+
+            
 
 
-            for (int i = 2;i <= 20; i += 2)                                                                                   // 2. Чётные числа
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine(" ");
-
-            for (int i = 1; i <= 5; i++)                                                                                      // 3. Таблица умножения
-            {
-                for (int j = 1; j <= 5; j++)
-                {
-                    Console.Write($"{i,2} * {j,2} = {i * j,2}  ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine(" ");
-
-            string password = "qwerty";
-            string userInput;
-
-            do
-            {
-                Console.Write("Введите пароль: ");                                                                              // 4. Парольы                                                                               
-                userInput = Console.ReadLine();
-                if (userInput != password)
-                {
-                    Console.WriteLine("Неверный пароль! Попробуйте снова.");
-                }
-            } while (userInput != password);
-            Console.WriteLine("Пароль верный");
         }
-
-
-
-
-
     }
-
-
 }
